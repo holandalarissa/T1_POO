2a80bac5d5a77ac00cce01cab45a802097fc002e
Larissa <95726877+holandalarissa@users.noreply.github.com>
2022-09-17 11:20:54 -0300

    Update Principal.java

--- Questão 04/src/Principal/Principal.java
+++ Questão 04/src/Principal/Principal.java
@@ -1,14 +1,13 @@
-package Principal; //Quest„o 04
+package Principal; //Quest√£o 04
 import javax.swing.JOptionPane;
 public class Principal {
-	
 	public static void main(String[] args) {
-		String msg = "Que tal um aumento de 25%?";
-		JOptionPane.showInternalMessageDialog(null, msg);
-		int sal = Integer.parseInt(JOptionPane.showInputDialog("Digite o seu sal·rio antigo:"));
-		int salnovo = (sal + sal *25/100);
-		String msg1 = "Seu novo sal·rio È:" + salnovo;
-		JOptionPane.showInternalMessageDialog(null, msg1);
+	String msg = "Que tal um aumento de 25%?";
+	JOptionPane.showInternalMessageDialog(null, msg);
+	int sal = Integer.parseInt(JOptionPane.showInputDialog("Digite o seu sal√°rio antigo:"));
+	int salnovo = (sal + sal *25/100);
+	String msg1 = "Seu novo sal√°rio √©:" + salnovo;
+	JOptionPane.showInternalMessageDialog(null, msg1);
 	}
 	
 }
